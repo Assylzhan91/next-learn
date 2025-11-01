b18ff48ac3fe6cda0927914c5738c9288f45f444
Асылжан Баймуратов <assylzhan.baimuratov@dmms.kz>
2025-11-01 23:10:30 +0500

    feat(app): 1. Add prettier
    Add prettier and commitzen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
-import React, {JSX} from "react";
-
+import React, { JSX } from 'react';
+import Head from 'next/head';
 
 export default function Home(): JSX.Element {
   return (
-    <div>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos, voluptatibus!
-    </div>
+    <>
+      <Head>
+        <title>My Custom Page Title</title>
+        <meta name="description" content="A description for my page." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos,
+        voluptatibus!
+      </div>
+    </>
   );
 }
