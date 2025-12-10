05e1eb545fc52f380b4b08a65c661c58f8e8c9bf
Асылжан Баймуратов <assylzhan.baimuratov@dmms.kz>
2025-12-10 09:24:13 +0500

    feat(footer): add content on the footer

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,6 +1,15 @@
 import React, { JSX } from 'react';
 import { FooterProps } from './Footer.props';
+import styles from './Footer.module.scss';
+import cn from 'classnames';
+import { format } from 'date-fns';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <footer {...props}>Footer</footer>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div>Lorem ipsum dolor sit amet @2020- {format(new Date(), 'yyyy')}</div>
+      <div>Lorem ipsum.</div>
+      <div>Lorem ipsum dolor.</div>
+    </footer>
+  );
 };
