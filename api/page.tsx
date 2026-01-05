8263dc77c84a5a6ea3c65097079231940c78fc18
Assylzhan <asylzhan.baimuratov@gmail.com>
2026-01-05 11:45:02 +0500

    feat(products): add getPage API

---
+++ api/page.tsx
@@ -0,0 +1,12 @@
+import { MenuItem } from '../interfaces/menu.interfaces';
+
+import { API } from '@/app/(site)/api';
+import { TopPageModel } from '../interfaces/page.interface';
+
+export async function getPage(alias: string): Promise<TopPageModel | null> {
+  const res = await fetch(API.topPage.byAlias + alias);
+  if (!res.ok) {
+    return null;
+  }
+  return res.json();
+}
--- src/app/(site)/products/[alias]/page.tsx
+++ src/app/(site)/products/[alias]/page.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
+import {notFound} from 'next/navigation';
 import React, { JSX } from 'react';
 import { HTag } from '@components';
+import { getPage } from '../../../../../api/page';
 
 export const metadata = { title: 'PageProducts' };
 
@@ -12,13 +14,13 @@ interface IPageProducts {
 
 export default async function Products({ params }: IPageProducts): Promise<JSX.Element>  {
   const { alias } = await params;
-  console.log(1);
+  const page = await getPage(alias);
+  if (!page) return notFound();
+
   return (
     <div>
     <HTag tag="h1">PageProducts</HTag>
-    <HTag tag="h1">{ alias }</HTag>
-
-
+    <HTag tag="h1">{page.title }</HTag>
     </div>
   );
 }
